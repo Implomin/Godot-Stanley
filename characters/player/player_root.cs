5978fcaf0826303ed68ef17b93d905ac48d492db
Implomin <Implomin@gmail.com>
2024-03-20 10:08:55 +0100

    0.0.1 minor changes

--- characters/player/player_root.cs
+++ characters/player/player_root.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 using System;
 using System.Collections.Generic;
 
@@ -8,17 +9,24 @@ public partial class player_root : CharacterBody3D
 	public const float JumpVelocity = 4.5f;
 	public Vector2 inputVector; 
 	[Signal] public delegate void SetMovementStateEventHandler(movementState movementState);
+	[Signal] public delegate void SetMovementDirectionEventHandler(movementState movementState);
+
 	[Export] Dictionary movementStates;
 
+	float sprintingVal = 1;
+	[Export] float sprintMultiplier = 1.3f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+
+    public override void _Ready()
+    {
+        EmitSignal(SignalName.SetMovementState, movementStates["stand"]);
+    }
 
     public override void _Input(InputEvent @event)
     {
 		if (Input.IsActionPressed("movement")){
 			inputVector = Input.GetVector("movement_right", "movement_left", "movement_backward", "movement_forward");
-			if (inputVector != Vector2.Zero){
+			if (DirectionalMovementKeyPressed()){
 				if (Input.IsActionPressed("movement_sprint")){
 					EmitSignal(SignalName.SetMovementState, movementStates["sprint"]);
 				}
@@ -30,5 +38,21 @@ public partial class player_root : CharacterBody3D
 		else{
 			EmitSignal(SignalName.SetMovementState, movementStates["stand"]);
 		}
+	}
+	
+	public override void _PhysicsProcess (double delta)
+	{
+		if(!IsOnFloor()){
+			EmitSignal(SignalName.SetMovementState, movementStates["airborne"]);
+		}
+
+		if(DirectionalMovementKeyPressed()){ 
+			EmitSignal(SignalName.SetMovementDirection, new Vector3(inputVector.X, 0, inputVector.Y));
+		}
+	}
+
+	private bool DirectionalMovementKeyPressed()
+	{
+		return inputVector != Vector2.Zero;
 	}
 }
