9094d689c94fd6d2ec5d1e50c2fb0cbda788c4e9
jakubciszak <jakub.ciszak@tallence.com>
2024-04-09 10:45:05 +0200

    revert to legacy

--- voiceover/src/main.rs
+++ voiceover/src/main.rs
@@ -1,3 +1,18 @@
+mod server;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    
+
+    let listener = TcpListener::bind("127.0.0.1:5051").expect("Failed creating a listner");
+    println!("LISTENING ON 127.0.0.1:5051");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                std::thread::spawn(|| server::handle_client(stream));
+            }
+            Err(e) => {
+                eprintln!("FAILED TO ESTABLISH CONNECTION: {}", e)
+            }
+        }
+    }
 }
