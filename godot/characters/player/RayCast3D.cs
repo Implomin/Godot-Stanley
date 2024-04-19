f948df2c6053af31b47ef49fea82e0e5917a27b0
Implomin <Implomin@gmail.com>
2024-04-19 09:29:32 +0200

    reworked pickup logic and switch to jolt for physics

--- godot/characters/player/RayCast3D.cs
+++ godot/characters/player/RayCast3D.cs
@@ -5,38 +5,52 @@ public partial class RayCast3D : Godot.RayCast3D
 {
 
 	[Export] Node3D pickUpObjLocation;
-	Pickupable lookingAtObject = new Pickupable(); 
+	Pickupable lookingAtObject = null; 
+	bool currentlyPickingUp = false;
+	bool isColliding = false;
 
 
 	public override void _Process(double delta) {
-		if (IsColliding()) {
+		if(IsColliding() && lookingAtObject == null){
 			lookingAtObject = (Pickupable)GetCollider();
-
-			if(Input.IsActionJustPressed("interaction_pickup"))
-			{
-				lookingAtObject.isPickedUp = !lookingAtObject.isPickedUp;
-				GD.Print("Picked up: " + lookingAtObject.isPickedUp);
-				//lookingAtObject.LinearVelocity = Vector3.Zero;
-			}
-		}else{
-			lookingAtObject.isPickedUp = false;
+			GD.Print("obj detected: " + lookingAtObject.Name);
 		}
 
-		if(lookingAtObject.isPickedUp){
-			lookingAtObject.GravityScale = 0f;
+		if(lookingAtObject != null){
+			if(currentlyPickingUp){
+				lookingAtObject.GravityScale = 0f;
+				lookingAtObject.LinearVelocity = (pickUpObjLocation.GlobalPosition - lookingAtObject.GlobalPosition)*15;
+				lookingAtObject.AngularVelocity = pickUpObjLocation.GlobalRotation - lookingAtObject.GlobalRotation;
+
+				if(Input.IsActionJustPressed("interaction_pickup")){
+					lookingAtObject.isPickedUp = false;
+					currentlyPickingUp = false;
+					GD.Print("Dropped object");
+				}
+			}else{
+				lookingAtObject.GravityScale = 1f;
+
+				if(IsColliding()){
+					lookingAtObject = (Pickupable)GetCollider();
+					GD.Print("obj detected: " + lookingAtObject.Name);
+
+					if(Input.IsActionJustPressed("interaction_pickup")){
+						lookingAtObject.isPickedUp = true;
+						currentlyPickingUp = true;
+						GD.Print("Picked up object");
+						
+					}else{
+						lookingAtObject = null;
+						GD.Print("No obj detected");
+					}
+				}
+			}
+		}
+
 /* 			lookingAtObject.GlobalPosition = lookingAtObject.GlobalPosition.Lerp(pickUpObjLocation.GlobalPosition, 0.5f);
 			lookingAtObject.GlobalRotation = lookingAtObject.GlobalRotation.Lerp(pickUpObjLocation.GlobalRotation, 0.5f); */
-			lookingAtObject.LinearVelocity = (pickUpObjLocation.GlobalPosition - lookingAtObject.GlobalPosition)*15;
-			lookingAtObject.AngularVelocity = pickUpObjLocation.GlobalRotation - lookingAtObject.GlobalRotation;
-
 			//lookingAtObject.GlobalRotation = lookingAtObject.GlobalRotation.Lerp(pickUpObjLocation.GlobalRotation, 0.5f);
-
 			//lookingAtObject.GlobalTransform = pickUpObjLocation.GlobalTransform;
-		}
-		else
-		{
-			lookingAtObject.GravityScale = 1f;
-		}
 
 	}
 }
